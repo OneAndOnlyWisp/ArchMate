e42bef06eb1a5df6a8fe29ed135e78c66ff1bd2e
Wisp <4ndr4cy@gmail.com>
2018-04-21 20:26:33 +0000

    Add files via upload

--- Kernel.sh
+++ Kernel.sh
@@ -56,15 +56,27 @@ function ReadBootCFG {
 }
 
 ReadBootCFG
+
+#ACTIVE_KERNEL
+[[ $(uname -r) = *"lts"* ]] && ACTIVE_KERNEL="Longterm" || ACTIVE_KERNEL="Stable"
+#DEFAULT_KERNEL
+DEFAULT_KERNEL=$(sed -n "${VM_Linuz_default[1]}p" $BootFile | sed 's/.*\///' | sed 's/\s.*$//')
+case $DEFAULT_KERNEL in
+  *"ck"* ) echo "CK kernel";;
+  *"lqx"* ) echo "Liquorix kernel";;
+  * ) [[ $DEFAULT_KERNEL = *"lts"* ]] && DEFAULT_KERNEL="Longterm" || DEFAULT_KERNEL="Stable"
+    ;;
+esac
+
+
 #Set default kernel to load                                         UUID_Stash Set here ˇ
 #sh Functions.sh ReplaceLineByNumber ${VM_Linuz_default[1]} "$(sed -n -e "${UUID_Stash[1]}p" $BootFile | sed 's/\//\\\//g' | cut -c 2-)" $BootFile
 #Set default kernel to load                                         IMG_Stash Set here ˇ
 #sh Functions.sh ReplaceLineByNumber ${VM_Linuz_default[2]} "$(sed -n -e "${IMG_Stash[1]}p" $BootFile | sed 's/\//\\\//g' | cut -c 2-)" $BootFile
 
 
-
 #Gather required system information
-[[ $(uname -r) = *"lts"* ]] && KERNEL_VERSION="LTS" || KERNEL_VERSION="default"
+
 #Menu
 while [ "$INPUT_OPTION" != "end" ]
 do
@@ -79,12 +91,15 @@ do
   echo "Version stash:" ${Version_Stash[*]}
   echo "UUID stash:" ${UUID_Stash[*]}
   echo "IMG stash:" ${IMG_Stash[*]}
-  sed -n "${VM_Linuz_default[1]}p" $BootFile
+  echo ""
+  #TEST
+  echo "Active kernel:" $ACTIVE_KERNEL
+  echo "Grub default kernel:" $DEFAULT_KERNEL
+  echo ""
   #-----------------------------------------------------------------------------
-  echo "Linux currently uses \"$KERNEL_VERSION\" kernel. (Press \"ESC\" to go back.)"
   echo "Available kernel options:"
-  case $KERNEL_VERSION in
-    "default")
+  case $ACTIVE_KERNEL in
+    "Stable")
       echo "1. Change to LTS Kernel."
       read -sn1 INPUT_OPTION
       case $INPUT_OPTION in
@@ -97,7 +112,7 @@ do
         $'\e') break;;
       esac
       ;;
-    "LTS")
+    "Longterm")
       echo "1. Change to default Kernel."
       read -sn1 INPUT_OPTION
       case $INPUT_OPTION in
