5763acd307a97de5df290521ce2294fbf1ae5fef
Wisp <4ndr4cy@gmail.com>
2018-04-21 19:41:37 +0200

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -15,16 +15,28 @@ INPUT_OPTION=default
 while [ "$INPUT_OPTION" != "end" ]
 do
   #Menu
-	echo "Please type the number of selected task! (Press \"ESC\" to quit.)"
-	echo "1. Kernel"
+  echo "Please type the number of selected task! (Press \"ESC\" to quit.)"
+  echo "1. Kernel"
   echo "2. CPU"
   echo "3. GPU"
+  echo "4. "
   read -sn1 INPUT_OPTION
 
   case $INPUT_OPTION in
     '1') sh $KERNEL $ME; clear;;
     '2') sh $CPU; clear;;
     '3') sh $GPU; clear;;
+    '4')
+      #"Multimedia engine"
+      pacman -S --noconfirm pulseaudio pulseaudio-alsa xorg xorg-xinit
+      #Desktop
+      pacman -S --noconfirm plasma-desktop  
+      echo "exec startkde" > ~/.xinitrc
+      #Custom apps
+      pacman -S --noconfirm konsole dolphin chromium atom sddm    
+      #Apply services
+      systemctl enable sddm.service
+      ;;
     $'\e') clear; break;;
   esac
 done
