fdbfcb7f8138ca68a7e23ec134a4209cb702a16f
Wisp <4ndr4cy@gmail.com>
2018-05-04 10:50:01 +0000

    Add files via upload

--- Custom.sh
+++ Custom.sh
@@ -8,26 +8,11 @@ fi
 if ! [[ $(pacman -Qs pup-git) = "" ]]; then
   sudo pacman -Rs --noconfirm pup-git
 fi
+
 #Custom apps
-#pacman -S --noconfirm konsole dolphin chromium atom sddm
+#pacman -S --noconfirm konsole dolphin chromium atom transmission-qt sddm
 #Apply services
 #systemctl enable sddm.service
 
 echo "Done!"
 read -sn1
-
-#ACTIVE_KERNEL
-case  in
-  *"ck"* ) echo "CK kernel";;
-  *"lqx"* ) echo "Liquorix kernel";;
-  * ) [[ $ACTIVE_KERNEL = *"lts"* ]] && ACTIVE_KERNEL="Longterm" || ACTIVE_KERNEL="Stable"
-    ;;
-esac
-#DEFAULT_KERNEL
-case $(sed -n "${VM_Linuz_default[1]}p" $BootFile | sed 's/.*\///' | sed 's/\s.*$//') in
-  *"ck"* ) echo "CK kernel";;
-  *"lqx"* ) echo "Liquorix kernel";;
-  * ) [[ $DEFAULT_KERNEL = *"lts"* ]] && DEFAULT_KERNEL="Longterm" || DEFAULT_KERNEL="Stable"
-    ;;
-esac
-echo "ok"
