2e93df96120e917f280f0b579961002cee62e4b8
Wisp <4ndr4cy@gmail.com>
2018-05-13 11:57:17 +0000

    Add files via upload

--- Custom.sh
+++ Custom.sh
@@ -3,44 +3,6 @@ clear
 #Local globals------------------------------------------------------------------
 Source_Path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
 #-------------------------------------------------------------------------------
-#Wine install elements----------------------------------------------------------
-function CheckDependancy {
-  sh ""$Source_Path"Functions.sh" InstallPackages "wget";
-}
-
-function GenerateLocale {
-  StartingLine=$(sed -n '/#en_US\.UTF-8 UTF-8/=' /etc/locale.gen)
-  if ! [[ "$StartingLine" = "" ]]; then
-    sed -ie ""$StartingLine"s/#//g" /etc/locale.gen
-    locale-gen
-  fi
-}
-
-function InstallFonts {
-  sh ""$Source_Path"Functions.sh" InstallPackages "ttf-liberation";
-  sh ""$Source_Path"Functions.sh" InstallPackages "wqy-zenhei";
-}
-
-function InstallSteam {
-  GenerateLocale
-  InstallFonts
-}
-
-function InstallWINE {
-  sh ""$Source_Path"Functions.sh" InstallPackages "wine";
-  sh ""$Source_Path"Functions.sh" InstallPackages "wine_gecko";
-  sh ""$Source_Path"Functions.sh" InstallPackages "wine-mono";
-}
-#-------------------------------------------------------------------------------
-
-InstallWINE
-URL="https://steamcdn-a.akamaihd.net/client/installer/"
-FileName="SteamSetup.exe"
-wget -O $HOME/$FileName $URL$FileName
-wine "$HOME/$FileName"
-
-read -sn1
-exit
 
 #Custom apps
 pacman -S --noconfirm konsole dolphin chromium atom transmission-qt sddm
