e95986c7dff2ddd22abc4791a4fe3d7c07365c87
Wisp <4ndr4cy@gmail.com>
2018-05-13 00:06:49 +0000

    Add files via upload

--- Custom.sh
+++ Custom.sh
@@ -2,6 +2,27 @@
 clear
 Source_Path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
 
+function InstallSteam {
+  function GenerateLocale {
+    StartingLine=$(sed -n '/#en_US\.UTF-8 UTF-8/=' /etc/locale.gen)
+    if ! [[ "$StartingLine" = "" ]]; then
+      sed -ie ""$StartingLine"s/#//g" /etc/locale.gen
+      locale-gen
+    fi
+  }
+  function InstallFonts {
+    sh ""$Source_Path"Functions.sh" InstallPackages "ttf-liberation";
+    sh ""$Source_Path"Functions.sh" InstallPackages "wqy-zenhei";
+  }
+  GenerateLocale
+  InstallFonts
+  sh ""$Source_Path"Functions.sh" InstallPackages "steam"
+}
+
+InstallSteam
+read -sn1
+exit
+
 #Custom apps
 pacman -S --noconfirm konsole dolphin chromium atom transmission-qt sddm
 #Apply services
