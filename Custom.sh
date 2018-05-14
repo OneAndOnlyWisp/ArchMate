510e0c98c6fadc4d990cae1d27aa4b0cbf21c8d6
Wisp <4ndr4cy@gmail.com>
2018-05-14 00:31:29 +0000

    Add files via upload

--- Custom.sh
+++ Custom.sh
@@ -6,9 +6,20 @@ Source_Path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
 #AUR package manager
 sh ""$Source_Path"Functions.sh" InstallPackages "aurman"
 
+#Must have
+pacman -S --noconfirm plasma-pa; #Volume adjustment app
+pacman -S --noconfirm spectacle; #Print screen tool
+pacman -S --noconfirm deepin-image-viewer; #Image viewer
 #Custom apps
-pacman -S --noconfirm konsole dolphin chromium atom transmission-qt plasma-pa sddm
-#Apply services
+pacman -S --noconfirm konsole; #Terminal
+pacman -S --noconfirm dolphin; #File manager
+pacman -S --noconfirm chromium; #Browser
+pacman -S --noconfirm atom; #Text editor
+pacman -S --noconfirm transmission-qt; #Torrent
+pacman -S --noconfirm ark unrar; #Archive manager
+
+#Display manager service
+pacman -S --noconfirm sddm
 systemctl enable sddm.service
 
 #SDDM autologin
