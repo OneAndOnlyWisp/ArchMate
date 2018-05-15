0386326d83bb3fa9186bcfb2519a524a923aac39
Wisp <4ndr4cy@gmail.com>
2018-05-15 02:32:08 +0200

    Add files via upload

--- Custom.sh
+++ Custom.sh
@@ -6,13 +6,29 @@ Source_Path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
 #AUR package manager
 sh ""$Source_Path"Functions.sh" InstallPackages "aurman"
 
-#Must have
+#Display manager service
+pacman -S --noconfirm sddm
+systemctl enable sddm.service
+
+#SDDM autologin
+echo "[Autologin]
+User=wisp
+Session=plasma.desktop" > /etc/sddm.conf.d/autologin.conf
+
+#Set window system keyboard layout
+localectl set-x11-keymap hu
+
+#Sys tool
 pacman -S --noconfirm ksysguard; #Process manager
+pacman -S --noconfirm partitionmanager; #KDE partition manager
+
+#Life savers
 pacman -S --noconfirm powerdevil; #Power management system tool
 pacman -S --noconfirm plasma-pa; #Volume adjustment app
 pacman -S --noconfirm spectacle; #Print screen tool
+
+#Default apps
 pacman -S --noconfirm gwenview; #Image viewer
-#Custom apps
 pacman -S --noconfirm konsole; #Terminal
 pacman -S --noconfirm dolphin; #File manager
 pacman -S --noconfirm atom; #Text editor
@@ -23,21 +39,6 @@ pacman -S --noconfirm kodi; #Cloud/Local Audio/Video player
 #Browser
 pacman -S --noconfirm chromium;
 echo "--password-store=basic" >> ~/.config/chromium-flags.conf
-
-#Display manager service
-pacman -S --noconfirm sddm
-systemctl enable sddm.service
-
-#SDDM autologin
-echo "[Autologin]
-User=wisp
-Session=plasma.desktop" > /etc/sddm.conf.d/autologin.conf
-#SDDM desktop theme
-echo "[Theme]
-Current=breeze-dark" > /etc/sddm.conf.d/theme.conf
-
-#Set window system keyboard layout
-localectl set-x11-keymap hu
 
 #Revert changes to makepkg
 cp ""$Source_Path"Assets/SysBU/makepkgBU" /usr/bin/makepkg
