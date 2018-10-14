a9ce3b0097bf3897d861c2a733f20e419fd66326
OneAndOnlyWisp <4ndr4cy@gmail.com>
2018-10-14 17:26:50 +0200

    "archived" gdb-gui

--- developer/main.sh
+++ developer/main.sh
@@ -5,23 +5,20 @@ CPP="$Source_Path/cpp.sh"
 DOCKER="$Source_Path/docker.sh"
 VULKAN="$Source_Path/vulkan.sh"
 TF="$Source_Path/tensorflow.sh"
-DEBUG="$Source_Path/debugger.sh"
 #Main loop----------------------------------------------------------------------
 while [ "$INPUT_OPTION" != "end" ]; do
   clear
   echo "Please type the number of selected task! (Press \"ESC\" to quit.)"
-  echo "1. Debugger (C, C++, Go, Rust)"
-  echo "2. C++"
-  echo "3. Docker"
-  echo "4. Vulkan"
-  echo "5. Tensorflow (Python)"
+  echo "1. C++"
+  echo "2. Docker"
+  echo "3. Vulkan"
+  echo "4. Tensorflow (Python)"
   read -sn1 INPUT_OPTION
   case $INPUT_OPTION in
-    '1') sh $DEBUG; clear;;
-    '2') sh $CPP; clear;;
-    '3') sh $DOCKER; clear;;
-    '4') sh $VULKAN; clear;;
-    '5') sh $TF; clear;;
+    '1') sh $CPP; clear;;
+    '2') sh $DOCKER; clear;;
+    '3') sh $VULKAN; clear;;
+    '4') sh $TF; clear;;
     $'\e') clear; break;;
   esac
 done
