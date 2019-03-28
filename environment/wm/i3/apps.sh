d22b4883b39f8cfe1effbecdc1da266539e12ca7
OneAndOnlyWisp <4ndr4cy@gmail.com>
2019-03-28 19:38:22 +0100

    Updated VNC support Updated comments

--- environment/wm/i3/apps.sh
+++ environment/wm/i3/apps.sh
@@ -7,28 +7,28 @@ functions="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../../functions.
 pacman -S --noconfirm ttf-dejavu ttf-inconsolata; # Default fonts
 pacman -S --noconfirm p7zip; # Extract .7z files
 pacman -S --noconfirm bc; # Floating point division (shell)
-pacman -S --noconfirm neofetch; #PC information
-pacman -S --noconfirm htop; #Process manager
-pacman -S --noconfirm rxvt-unicode urxvt-perls; #Terminal
-pacman -S --noconfirm pavucontrol; #PulseAudio Volume controller
-pacman -S --noconfirm scrot xclip; #Print screen tool/Copy to clipboard
-pacman -S --noconfirm pcmanfm-gtk3; #File manager
-pacman -S --noconfirm file-roller; #Archive manager
-pacman -S --noconfirm chromium; #Browser
-pacman -S --noconfirm atom; #Text editor
-pacman -S --noconfirm transmission-cli; #Torrent daemon
-pacman -S --noconfirm vlc; #Video player
-pacman -S --noconfirm bash-completion; #Super intelligent completion
-pacman -S --noconfirm kvantum-qt5; #Window system theme manager
-pacman -S --noconfirm udiskie; #USB Automount
-pacman -S --noconfirm gparted; #Partition manager
-pacman -S --noconfirm remmina; # VNC client
-pacman -S --noconfirm tigervnc; # VNC server (/client)
-sh $functions InstallFromAUR "aurman"; #AUR package installer
+pacman -S --noconfirm neofetch; # PC information
+pacman -S --noconfirm htop; # Process manager
+pacman -S --noconfirm rxvt-unicode urxvt-perls; # Terminal
+pacman -S --noconfirm pavucontrol; # PulseAudio Volume controller
+pacman -S --noconfirm scrot xclip; # Print screen tool/Copy to clipboard
+pacman -S --noconfirm pcmanfm-gtk3; # File manager
+pacman -S --noconfirm file-roller; # Archive manager
+pacman -S --noconfirm chromium; # Browser
+pacman -S --noconfirm atom; # Text editor
+pacman -S --noconfirm transmission-cli; # Torrent daemon
+pacman -S --noconfirm vlc; # Video player
+pacman -S --noconfirm bash-completion; #S uper intelligent completion
+pacman -S --noconfirm kvantum-qt5; # Window system theme manager
+pacman -S --noconfirm udiskie; # USB Automount
+pacman -S --noconfirm gparted; #P artition manager with GUI
+pacman -S --noconfirm remmina libvncserver freerdp; # VNC client
+pacman -S --noconfirm tigervnc; # VNC server for separate workspace
+pacman -S --noconfirm x11vnc; # VNC server to share workspace
+sh $functions InstallFromAUR "aurman"; # AUR package installer
 sh $functions InstallFromAUR "urxvt-resize-font-git"; # Resize font on fly
 sh $functions InstallFromAUR "ttf-font-awesome-4"; # Awesome 4 fonts
 sh $functions InstallFromAUR "ttf-font-logos"; # Arch linux logo
 #-------------------------------------------------------------------------------
 # pacman -S --noconfirm xfce4-panel; # Only for the weak
-# pacman -S --noconfirm x11vnc; # To share screen through VNC
 #-------------------------------------------------------------------------------
