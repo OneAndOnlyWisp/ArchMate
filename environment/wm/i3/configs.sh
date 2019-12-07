428c0e81ee51eb04f8fa3154ca9ff3f82099776f
OneAndOnlyWisp <4ndr4cy@gmail.com>
2019-12-07 15:40:35 +0100

    Changed cp ro rsync since folders cannot be created with cp

--- environment/wm/i3/configs.sh
+++ environment/wm/i3/configs.sh
@@ -23,7 +23,7 @@ sed -n '/\/bin\/bash/p' /etc/passwd | cut -d: -f1 | while read -r _USER; do
   # Set home folder location
   [[ $_USER = "root" ]] && _USER_HOME_DIR="/root" || _USER_HOME_DIR="/home/$_USER"
   # Copy config files
-  cp -r "$_USER_DIR/*" "$_USER_HOME_DIR/";
+  rsync -av "$_USER_DIR/" "$_USER_HOME_DIR/";
   # Fix blocklets permissions
   find "$_USER_HOME_DIR/.blocklets" -type f -exec chmod 700 {} \;
   # Fix ownership
@@ -41,4 +41,4 @@ cp "$_CUSTOM_DIR/wallpaper.png" /wallpaper.png;
 # Turn off beep sounds (Blacklist kernel module)
 echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf;
 #-------------------------------------------------------------------------------
-read -sn1 
+read -sn1
